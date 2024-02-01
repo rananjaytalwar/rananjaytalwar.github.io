9de12a0d4605c83d655d20a6b690a05df9003ba4
Rananjay <rananjay3516@gmail.com>
2024-02-01 19:41:07 +0530

    format script

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const container = document.querySelector('#home-page')
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
-var j = (Math.floor(Math.random() * 150)) + 1
+
+// random pokemon index
+let j = (Math.floor(Math.random() * 150)) + 1
+
 const pokemon = document.createElement('div')
+
 const newImg = document.createElement('img')
 newImg.src = `${baseURL}/${j}.png`
+
 pokemon.appendChild(newImg)
 container.appendChild(pokemon)
+
 pokemon.classList.add('pokemon')
-
-
-/*
-<div>
-    <img>
-    <span>2</span>
-</div>
-*/
