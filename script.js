0a9d8e1c8807f77d8570f3a0a02f3dcd04344d90
Rananjay <rananjay3516@gmail.com>
2024-02-02 14:21:10 +0530

    dom load before script

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const container = document.querySelector('#home-page')
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('#home-page')
+    const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+    
+    // random pokemon index
+    let j = (Math.floor(Math.random() * 150)) + 1
+    
+    const pokemon = document.createElement('div')
+    
+    const newImg = document.createElement('img')
+    newImg.src = `${baseURL}/${j}.png`
+    
+    pokemon.appendChild(newImg)
+    container.appendChild(pokemon)
+    
+    pokemon.classList.add('pokemon')
+});
 
-// random pokemon index
-let j = (Math.floor(Math.random() * 150)) + 1
 
-const pokemon = document.createElement('div')
-
-const newImg = document.createElement('img')
-newImg.src = `${baseURL}/${j}.png`
-
-pokemon.appendChild(newImg)
-container.appendChild(pokemon)
-
-pokemon.classList.add('pokemon')
